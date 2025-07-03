305aebb1d0fbb31e54f655bf35df901f627f36b0
baldurael <aelbtajul@gmail.com>
2025-07-03 12:56:01 +0800

    igris-store

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// GANTI ISI CONFIG INI DENGAN PUNYA LO DARI FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSy....",
+  authDomain: "igris-store.firebaseapp.com",
+  projectId: "igris-store",
+  storageBucket: "igris-store.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcd"
+};
+
+firebase.initializeApp(firebaseConfig);
